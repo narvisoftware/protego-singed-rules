407140d1246a09470d71e70e2036e0eced1a6d81
Mihai Vasilache <mihai.vasilache@gmail.com>
2024-03-22 22:47:03 +0200

    test

--- src/main/java/app/narvi/authz/auditlog/LoggingAuditProvider.java
+++ src/main/java/app/narvi/authz/auditlog/LoggingAuditProvider.java
@@ -2,13 +2,12 @@ package app.narvi.authz.auditlog;
 
 import java.lang.invoke.MethodHandles;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import app.narvi.authz.AuditProvider;
 import app.narvi.authz.Permission;
 import app.narvi.authz.PolicyRule;
-import app.narvi.authz.SecurityContext;
+import app.narvi.authz.PolicyRule.Decision;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class LoggingAuditProvider implements AuditProvider {
@@ -16,7 +15,9 @@ public class LoggingAuditProvider implements AuditProvider {
   private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   @Override
-  public void audit(Permission permission, PolicyRule policyRule, SecurityContext securityContext) {
-
+  public void audit(Permission permission, PolicyRule policyRule, Decision decision) {
+    LOG.info("Audit: Rule " + policyRule.getClass() + " result:" + decision.name() + " for " + permission.getClass()
+        .getSimpleName());
   }
+
 }
--- src/test/java/app/narvi/example/ExampleTest.java
+++ src/test/java/app/narvi/example/ExampleTest.java
@@ -3,24 +3,31 @@ package app.narvi.example;
 import app.narvi.authz.BasicPolicyRuleProvider;
 import app.narvi.authz.Permission;
 import app.narvi.authz.PolicyEvaluator;
-import app.narvi.authz.PolicyRule.Decision;
+import app.narvi.authz.PolicyException;
 import app.narvi.authz.PolicyRulesProvider;
 import app.narvi.authz.rules.AllowNothingPolicyRule;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import app.narvi.authz.PolicyException;
 
 public class ExampleTest {
 
 
   @Test
   public void example1() {
-    PolicyRulesProvider policyRulesProvider = BasicPolicyRuleProvider.of(new AllowNothingPolicyRule(), new TestNotPermitRule());
+    PolicyRulesProvider policyRulesProvider = BasicPolicyRuleProvider.of(new AllowNothingPolicyRule(),
+        new TestNotPermitRule());
     PolicyEvaluator.registerProviders(policyRulesProvider);
+    FakePermission dummyPermission = new FakePermission(new Object(), new Object());
     Throwable exception = Assertions.assertThrows(PolicyException.class,
-        () -> PolicyEvaluator.evaluatePermission(new Permission(new Object(), new Object())));
+        () -> PolicyEvaluator.evaluatePermission(dummyPermission));
     Assertions.assertEquals(PolicyException.class, exception.getClass());
     //System.out.println(result);
+  }
+
+  public class FakePermission extends Permission {
+    public FakePermission(Object protectedResource, Object action) {
+      super(protectedResource, action);
+    }
   }
 
 }
